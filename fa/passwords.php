1a956f583e35ce4bf46c4a7dab2cdcc795101674
mojtaba rakhisi <mojtaba.street@gmail.com>
2018-06-27 10:11:43 +0430

    adding validation messages

---
+++ fa/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'رمز عبور باید حداقل 6 کاراکتر و تایید شده باشد',
+    'reset' => 'رمز عبور شما با موفقیت تغییر یافت!',
+    'sent' => 'لینک تغییر رمز عبور برای شما فرستاده شد!',
+    'token' => 'token  تغییر گذر واژه (لینک) نامعتبر است.',
+    'user' => "کاربری با این ایمیل یافت نشد.",
+
+];
